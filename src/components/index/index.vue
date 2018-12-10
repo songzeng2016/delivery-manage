a452f17d9bd91c4382383f1b7fe53d1093bfd206
songzeng2016 <songzeng62@gmail.com>
2018-12-10 17:57:59 +0800

    'update'

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -6,29 +6,23 @@
     <el-container>
       <el-aside width="200px">
         <el-menu
-          default-active="2"
-          class="menu">
+          default-active="1-1"
+          class="menu"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>导航一</span>
+              <span>数据管理</span>
             </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
+            <el-menu-item index="1-1">商品列表</el-menu-item>
+            <el-menu-item index="1-2">商户列表</el-menu-item>
+            <el-menu-item index="1-3">订单列表</el-menu-item>
           </el-submenu>
           <el-menu-item index="2">
             <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
+            <span slot="title">添加数据</span>
           </el-menu-item>
           <el-menu-item index="3" disabled>
             <i class="el-icon-document"></i>
