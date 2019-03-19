a184ff4207fbb6cead72050b9572929343300644
songzeng2016 <songzeng62@gmail.com>
2019-03-19 18:35:00 +0800

    ''

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,7 +3,7 @@ import qs from 'qs';
 import router from '../router';
 import {Message} from 'element-ui';
 
-// axios.defaults.baseURL = 'http://songzeng1994.cn/api';
+axios.defaults.baseURL = 'http://songzeng1994.cn/api';
 
 // axios 响应拦截器
 axios.interceptors.response.use(res => {
